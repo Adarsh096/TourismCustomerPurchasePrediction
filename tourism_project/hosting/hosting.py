b3b74c3f62aeb06eb404781db1b99154431eafa4
Adarsh096 <adarshofficial11@gmail.com>
2026-02-05 16:47:53 +0000

    Visit With Us Project Files Update

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -14,7 +14,12 @@ try:
     print(f"Space created at: {repo_id}")
 except Exception as e:
     print(f"Space already exists or encountered an error: {e}")
+# SYNC VARIABLES
+# This sends the GitHub vars into the HF Space Environment
+api.add_space_variable(repo_id=repo_id, key="HUGGINGFACE_USER_NAME", value=HUGGINGFACE_USER_NAME)
+api.add_space_variable(repo_id=repo_id, key="HUGGINGFACE_MODEL_NAME", value=os.getenv('HUGGINGFACE_MODEL_NAME'))
 
+#upload the streamlit app files:
 api.upload_folder(
     folder_path="tourism_project/deployment",
     repo_id=repo_id,  # the target repo
